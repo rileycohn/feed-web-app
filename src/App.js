343e47c90e9f77d472fa46e76654e7b91824c225
rubenBojorquez <rbojorq4@asu.edu>
2021-03-18 11:53:34 -0700

    Adding Post component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
-  return (
-    <>
-    <AmplifySignOut button-text="Sign Out"/>
-    </>
-  );
+return(
+  <>
+  <AmplifySignOut button-text="Sign Out"/>
+  </>
+);
 }
 
 export default withAuthenticator(App);
