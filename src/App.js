72d6ebbebe79e1eb0240e4da39d23a5afe944417
rubenBojorquez <79663969+rubenBojorquez@users.noreply.github.com>
2021-03-19 01:10:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hey!
-          Learn React 3
-        </a>
-      </header>
-    </div>
+    <>
+    <AmplifySignOut button-text="Sign Out"/>
+    </>
   );
 }
 
