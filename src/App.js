215e173faa6269255066875a0746d0916824483f
rubenBojorquez <rbojorq4@asu.edu>
2021-11-20 14:56:51 -0700

    import API

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Feed from './Feed';
 import Widgets from './Widgets';
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import API from '@aws-amplify/api';
 
 function App() {
+
   return (
     <div className="app">
       <AmplifySignOut button-text="Sign Out"/>
